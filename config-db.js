70b0709437a0e93eed83d2df030fce0d49c641ce
anas demnati <anas.demnati2@gmail.com>
2016-08-19 16:19:56 +0200

    added ip + port + uri

--- config-db.js
+++ config-db.js
@@ -8,4 +8,24 @@ const heroToursDB = mongoose.connect("mongodb://otawara:rouidate159357@ds063715.
     console.log(`The connection has been established on mongodb://localhost/hero-tours`);
 });
 
-exports.heroToursDB = heroToursDB;
+module.exports = {
+    heroToursDB: heroToursDB,
+
+    // Server IP
+    ip:       process.env.OPENSHIFT_NODEJS_IP ||
+            process.env.IP ||
+            undefined,
+
+    // Server port
+    port:     process.env.OPENSHIFT_NODEJS_PORT ||
+            process.env.PORT ||
+            '11432',
+
+    // MongoDB connection options
+    mongo: {
+    uri:    'mongodb://otawara:rouidate159357@ds063715.mlab.com:63715/hero-tours'
+            // process.env.MONGOLAB_URI ||
+            // process.env.MONGOHQ_URL ||
+            // process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+    }
+};
